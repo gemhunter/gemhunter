a9a42028940e77c9c196f0fc309e7dd4a4740ccf
Dhruv Singal <singaldhruv.ds@gmail.com>
2015-03-28 20:19:21 +0530

    Added more test files

---
+++ bin/untilCode.rb
@@ -0,0 +1,8 @@
+a = 1
+until a >= 20
+  a+=1
+  if a<15
+    a+=2
+    redo
+  end
+end
--- bin/whileCode.rb
+++ bin/whileCode.rb
@@ -1,10 +1,8 @@
 a = 1
 b = 2
-x = true
-z = false
-z = a < b && a + 2 > b
-x = a <= b  || a >= b
-
 while a<20
   a += 1
+  if a > 15
+    next
+  end
 end
