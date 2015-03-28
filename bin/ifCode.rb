8912900b03e8a6f9c857b2ee4027601dc8697645
Dhruv Singal <singaldhruv.ds@gmail.com>
2015-03-28 20:08:09 +0530

    Added some basic test files

---
+++ bin/ifCode.rb
@@ -0,0 +1,12 @@
+a = 1
+b = 2
+x = true
+z = false
+z = a < b && a + 2 > b
+x = a <= b  || a >= b
+if x then
+  a = 2
+elsif z
+  b = 2
+else c = a+b
+end
---
+++ bin/whileCode.rb
@@ -0,0 +1,10 @@
+a = 1
+b = 2
+x = true
+z = false
+z = a < b && a + 2 > b
+x = a <= b  || a >= b
+
+while a<20
+  a += 1
+end
