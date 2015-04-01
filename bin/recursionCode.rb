32f53fbaa2d328f520a0f5b76f5115eeb05cc703
infiniteloop <aksagg@iitk.ac.in>
2015-04-02 03:56:22 +0530

    Tested recursion

---
+++ bin/recursionCode.rb
@@ -0,0 +1,19 @@
+def Int m1(Int x)
+	if x < 0 then
+		return 0
+	end
+	def Int m3(Int x)
+		return x*x*x
+	end
+	def Int m2(Int x)
+		if x < 0 then
+			return x*x*m1(x-1)
+		end
+		def Int m4(Int x)
+			return m4(x)
+		end
+		return m4(x+2)
+	end
+	return m2(x) + m3(x) + m1(x-1)
+end
+m1(5)
--- bin/runall.sh
+++ bin/runall.sh
@@ -5,5 +5,5 @@
 ./irgen.py ifCode.rb
 ./irgen.py classCode.rb
 ./irgen.py methodCode.rb
-./irgen.py methodCode2.rb
+./irgen.py recursionCode.rb
 ./irgen.py tryCode.rb
