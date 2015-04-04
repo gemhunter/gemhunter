9b0a62c2875f242e119680f6f3c20b9fa40a5b03
infiniteloop <aksagg@iitk.ac.in>
2015-04-04 20:50:50 +0530

    Shifted test files to test folder

--- bin/runall.sh
+++ bin/runall.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-./irgen.py exprCode.rb
-./irgen.py untilCode.rb
-./irgen.py whileCode.rb
-./irgen.py ifCode.rb
-./irgen.py classCode.rb
-./irgen.py methodCode.rb
-./irgen.py recursionCode.rb
-./irgen.py arrayCode.rb
-./irgen.py tryCode.rb
+./irgen.py ../test/exprCode.rb
+./irgen.py ../test/untilCode.rb
+./irgen.py ../test/whileCode.rb
+./irgen.py ../test/ifCode.rb
+./irgen.py ../test/classCode.rb
+./irgen.py ../test/methodCode.rb
+./irgen.py ../test/recursionCode.rb
+./irgen.py ../test/arrayCode.rb
+./irgen.py ../test/tryCode.rb
