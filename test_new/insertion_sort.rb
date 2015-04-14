e7a2587c08bcececebecc72bdb63257ad75fb12a
adityamodi <adityamodi94@gmail.com>
2015-04-14 23:24:55 +0530

    insertion sort test file

--- test_new/insertion_sort.rb
+++ test_new/insertion_sort.rb
@@ -8,15 +8,17 @@ end
 
 def Void insertion_sort()
 	for i in 0..(len($a)-1)
-		x = $a[i]
 		j = i
 		while j>0 && $a[j-1]>$a[i]
+			temp = $a[j]
 			$a[j] = $a[j-1]
+			$a[j-1] = temp
 			j -= 1
 		end
-		$a[j] = x
 	end
 end
+
+insertion_sort()
 
 for i in 0..(len($a)-1)
 	puts $a[i]
